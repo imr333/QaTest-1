af9fd0ad113ad47086176e25d377b6bf2c144408
imr333 <rror666@mail.ru>
2024-06-22 01:07:50 +0300

    update test

--- src/test/java/ButtonClicks.java
+++ src/test/java/ButtonClicks.java
@@ -1,11 +1,13 @@
+import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.clickable;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.files.DownloadActions.click;
@@ -17,12 +19,21 @@ public class ButtonClicks {
         Configuration.baseUrl = ("https://webdriveruniversity.com");
         Configuration.browser = ("Chrome");
         Configuration.browserSize = ("1920x1080");
-        Configuration.holdBrowserOpen = true;
+    }
+
+    //по хорошему нужно сделать две переменные на закрытие модалки и проверку хедера, но задача попробовать несколько вариантов взаимодействия с элементами:
+    private final SelenideElement closeBtn = $(".fade.in.show .btn-default");
+    private final SelenideElement modalTitle = $(".modal.fade.in.show .modal-header");
+
+
+    @BeforeEach
+    public void openBrowser() {
+        open("/Click-Buttons/index.html");
+
     }
 
     @Test
     void testButtonClick() {
-        open("/Click-Buttons/index.html");
 
         //Клик по кнопке №1 с помощью xpath селектора, проверка модального окна и его закрытие
         $x("//*[@id='button1']").click();
@@ -31,17 +42,25 @@ public class ButtonClicks {
 
         //Клик по кнопке №2 с помощью JS с дальнейшим закрытием модального окна
         SelenideElement button2 = $("#button2");
-        Selenide.executeJavaScript("arguments[0].click()", button2);
-        SelenideElement close = $("#myModalJSClick > .modal-dialog > .modal-content > .modal-footer > .btn");
-        $(close).click();
-//      Selenide.executeJavaScript("arguments[0].click()", close); //хз почему, с помощью JS кликнуть на кнопку не получается
+        button2.click(ClickOptions.usingJavaScript()); // Selenide.executeJavaScript("arguments[0].click()", button2);
+        modalTitle.shouldHave(text("Well I think it is....."));
+        SelenideElement close = $("#myModalJSClick .modal-footer > .btn");
+        close.click(ClickOptions.usingJavaScript()); // Selenide.executeJavaScript("arguments[0].click()", close);
+//
 
-        //Клик по кнопке №3 с предварительным наведением
+        //Клик по кнопке №3 с помощью метода actions()
+        /*поскольку тестовый сайт забагован - исопльзовался клик с помощью JS*/
         SelenideElement button3 = $("#button3");
-        actions().moveToElement(button3,10,10).click().perform(); //хелп, не могу тыкнуть по кнопке... (как только не пробовал)
+        button3.click(ClickOptions.usingJavaScript());
+//        actions().moveToElement(button3).click(button3).perform(); //метод не работает из-за бага на сайте
+        modalTitle.shouldHave(text("Action Move & Click"));
+        closeBtn.shouldBe(visible).click(ClickOptions.usingJavaScript());
+    }
 
-
-
+    @AfterEach
+    public void tearDown(){
+        sleep(3000);
+        closeWebDriver();
     }
 
 }
