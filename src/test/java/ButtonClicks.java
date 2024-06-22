4f2fc51e719b50f42fce651b973591b62e30e1b9
imr333 <rror666@mail.ru>
2024-06-22 12:33:33 +0300

    final update test

--- src/test/java/ButtonClicks.java
+++ src/test/java/ButtonClicks.java
@@ -2,10 +2,7 @@ import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.by;
@@ -34,27 +31,35 @@ public class ButtonClicks {
     }
 
     @Test
-    void testButtonClick() {
-
+    @DisplayName("WebElement Click")
+    public void clickViaWebDriver() {
         /*Клик по кнопке №1 с помощью xpath селектора, проверка модального окна и его закрытие*/
-        $x("//*[@id='button1']").click();
+        $x("//*[@id='button1']").shouldBe(visible).click();
         $x("//*[@class='modal-title']").shouldHave(text("Congratulations!"));
-        $x("//*[@data-dismiss='modal']").click();
+        $x("//*[@data-dismiss='modal']").shouldBe(visible).click();
+    }
 
+    @Test
+    @DisplayName("JavaScript Click")
+    public void clickViaJS() {
         /*Клик по кнопке №2 с помощью JS с дальнейшим закрытием модального окна*/
         SelenideElement button2 = $("#button2");
         button2.click(ClickOptions.usingJavaScript()); // Selenide.executeJavaScript("arguments[0].click()", button2);
         modalTitle.shouldHave(text("Well I think it is....."));
         SelenideElement close = $("#myModalJSClick .modal-footer > .btn");
         close.click(ClickOptions.usingJavaScript()); // Selenide.executeJavaScript("arguments[0].click()", close);
+    }
 
-        /*Клик по кнопке №3 с помощью метода actions()
-        поскольку тестовый сайт забагован - исопльзовался клик с помощью JS*/
+    @Test
+    @DisplayName("Action Move & Click")
+    public void clickViaActions() {
+        /*Клик по кнопке №3 с помощью метода actions()*/
         SelenideElement button3 = $("#button3");
-        button3.click(ClickOptions.usingJavaScript());
-//        actions().moveToElement(button3).click(button3).perform();
+//        button3.click(ClickOptions.usingJavaScript());
+        actions().moveToElement(button3).click(button3).perform();
         modalTitle.shouldHave(text("Action Move & Click"));
-        closeBtn.shouldBe(visible).click(ClickOptions.usingJavaScript());
+        actions().moveToElement(closeBtn).click(closeBtn).perform();
+//        closeBtn.shouldBe(visible).click(ClickOptions.usingJavaScript());
     }
 
     @AfterEach
